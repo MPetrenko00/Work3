69c8612842e63654de3fa639faec4741dd2c04ac
m.petrenko <mandarinkashusha@gmail.com>
2023-06-14 14:15:13 +0300

    setTimeout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 //Рекурсия
 
-function calcSumm(numOme, numTwo){
+/*function calcSumm(numOme, numTwo){
     if(numTwo === 1){
         return numOme;
     }else{
         return numOme * calcSumm(numOme,numTwo - 1);
     }
 }
-console.log(calcSumm(2,3))
+console.log(calcSumm(2,3))*/
+
+//Планирование setTimeout and setInterval
+
+function showNumber(num){
+    console.log(num);
+    if(num < 5){
+        setTimeout(showNumber, 1000, ++num);
+    }
+}
+setTimeout(showNumber, 1000, 1);
